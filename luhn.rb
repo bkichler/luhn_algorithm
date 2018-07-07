075626ff28e84b92ae95f17f61d2fe7adcf34ae0
Brian Kichler <brian.kichler@gmail.com>
2018-07-07 12:18:57 -0700

    Added rspec and wrote out tests

--- luhn.rb
+++
@@ -1,6 +0,0 @@
-# Step 1 - break the credit card into it's individual digits.
-# Step 2 - Start from the right, double every second digit (i.e digit 2, 4, 6 etc.).
-# Step 3 - If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
-# Step 4 - Sum the digits.
-# Step 5 - If the sum is divisible by 10 it's a valid number. Otherwise it's invalid.
-
